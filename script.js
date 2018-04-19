67fffad0e24fd6d4fe8ec58d4c2a89565e488d72
Dan Meredith <sassy@macbook.codefactoryvienna.local>
2018-04-19 11:40:15 +0200

    added complete intermediate and advanced 1+2

--- script.js
+++ script.js
@@ -24,3 +24,59 @@ document.write("<br>");
 fruits.pop("<strong>orange</strong>")
 document.write("after popping <strong>orange</strong> : "+ fruits);
 document.write("<br>");
+document.write("<hr>");
+
+var animals = ["monkey" , "horse" , "dog"];
+document.write("before sorting the variable : "+ animals.join(', '));
+document.write("<br>");
+document.write("after sorting the variable : "+ animals.sort());
+document.write("<br>")
+animals.unshift("<strong>cat</strong>")
+document.write("after unshifting the variable : "+animals);
+document.write("<hr>")
+
+// var fruitsTwo = ["mango/cherries/kiwi/grapes/pear/peach/orange/lemon"]
+// document.write(fruitsTwo);
+// document.write("<br>");
+// fruitsTwo = fruitsTwo.replace(/\//g, "SHIT");
+// document.write(fruitsTwo)
+document.write("<hr>");
+document.write("<h1>Advanced Exercises</h1>");
+document.write("<hr>");
+var a = 7;
+var b = 14;
+var c = "21";
+var d = '36';
+var e = 42;
+var x = Number(c);
+var y = Number(d);
+var sum = a+b+x+y+e;
+document.write("All variables converted into integers and summed :" + sum);
+document.write("<br>")
+
+var f = '1';
+var g = 15;
+var h = 8;
+var i = "1";
+var n1 = Number(f);
+var n2 = Number(i);
+document.write("All variables converted into integers and multiplied :" + (mul = n1*g*h*n2));
+document.write("<br>");
+document.write("Both previous figures divided :" + (divide = sum/mul)); 
+document.write("<br>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
