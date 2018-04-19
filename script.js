70350d7fb3b65974f9039e00ce6be8eb79d3ffc6
alexhardo <alexhardo@msn.com>
2018-04-19 09:36:32 +0200

    first exercise is done

---
+++ script.js
@@ -0,0 +1,6 @@
+var players = ["Martin" , "Thomas", "Peter", "Mathias", "Niki"];
+document.write (players [2] + " is the best Player in the world <br>");
+document.write ("yes thats right <br>");
+
+console.log(players [2] + " is the best Player in the world <br>");
+console.log("yes thats right <br>");
